430ea6a58d950ff87947475b17874481a61f22fd
ZhangHanzhongFox <e1513113@u.nus.edu>
2026-09-17 17:34:37 +0800

    GUI: Complete tutorial part 3
    Handle text-field and button actions, show Duke responses, keep recent messages visible, and distinguish the two speakers.

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,4 +1,9 @@
+import java.util.Collections;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -24,5 +29,39 @@ public class DialogBox extends HBox {
         displayPicture.setFitHeight(100.0);
         setAlignment(Pos.TOP_RIGHT);
         getChildren().addAll(dialog, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box so Duke's image appears on the left.
+     */
+    private void flip() {
+        ObservableList<Node> children = FXCollections.observableArrayList(getChildren());
+        Collections.reverse(children);
+        getChildren().setAll(children);
+        setAlignment(Pos.TOP_LEFT);
+    }
+
+    /**
+     * Creates a dialog box for a user message.
+     *
+     * @param text message to display
+     * @param image user's image
+     * @return dialog box aligned for the user
+     */
+    public static DialogBox getUserDialog(String text, Image image) {
+        return new DialogBox(text, image);
+    }
+
+    /**
+     * Creates a dialog box for a Duke response.
+     *
+     * @param text response to display
+     * @param image Duke's image
+     * @return dialog box aligned for Duke
+     */
+    public static DialogBox getDukeDialog(String text, Image image) {
+        DialogBox dialogBox = new DialogBox(text, image);
+        dialogBox.flip();
+        return dialogBox;
     }
 }
